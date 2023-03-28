9e53829f4e458e3adf4e4c5bc276f63a352de9d1
joelallto <joelallto@gmail.com>
2023-03-28 15:17:15 -0300

    Lampada Interativa

---
+++ script.js
@@ -0,0 +1,12 @@
+function Ligar() {
+    var img = window.document.getElementById('imagem')
+    img.src = "assets/lampadaLig.svg";
+}
+function Desligar() {
+    var img = window.document.getElementById('imagem')
+    img.src = "assets/lampadaDesl.svg";
+}
+function Quebrar(){
+    var img = window.document.getElementById('imagem')
+    img.src = "assets/lampadaQue.svg";
+}
